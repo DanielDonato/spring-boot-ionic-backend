8f9e0314723cc375dc6b2989ce772cc9b7f073fa
DanielDonato <daniel_donato10@yahoo.com.br>
2019-05-12 18:22:35 -0300

    Produto associacao muitos pra muitos

--- src/main/java/com/danieldonato/application/Application.java
+++ src/main/java/com/danieldonato/application/Application.java
@@ -1,7 +1,9 @@
 package com.danieldonato.application;
 
 import com.danieldonato.application.domain.Categoria;
+import com.danieldonato.application.domain.Produto;
 import com.danieldonato.application.repositories.CategoriasRepository;
+import com.danieldonato.application.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -19,11 +21,28 @@ public class Application  implements CommandLineRunner {
 	@Autowired
 	private CategoriasRepository categoriasRepository;
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		Produto p1 = new Produto(null, "Computador", 2000.0);
+		Produto p2 = new Produto(null, "Impressora", 800.0);
+		Produto p3 = new Produto(null, "Mouse", 80.0);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+
 		categoriasRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
