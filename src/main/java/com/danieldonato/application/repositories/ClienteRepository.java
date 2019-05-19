12d9b7f046ba82d63f9417384c0da2a67ddc5686
DanielDonato <daniel_donato10@yahoo.com.br>
2019-05-19 17:19:21 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/danieldonato/application/repositories/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.danieldonato.application.repositories;
+
+import com.danieldonato.application.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+}
